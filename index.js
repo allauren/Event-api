a94699689642c4e69ce39489a4c2070f1b1ba8ac
allauren <alexislaurentprof@gmail.com>
2018-09-30 08:48:19 +0200

    added connection to database and listening on port, checked errors handling and managed to handle double listening error without crash

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const app = require('express')
+const express = require('express')
 const mongo = require('mongoose')
-const middlewares = require('middlewares')
+const keys = require('./config/keys')
+/* Connecting to database *\
+XXX CODE
+*/
 
+try {
+	mongo.connect(keys.mongoURL)
+	console.log('connection success')
+}
+catch (err) {
+	console.error(err);
+};
+
+
+const app = express()
+/* all the required requests */
+/*
 classe :
 Event :
 - name: nom de l'event fournis
 - cookieId: id cookie unique recupere ou fournis
-- referer: source http de l'event 
-- ip: source ip de l'event
-- createdAt: date de création de l'event
+- referer: source http de l'event fournis
+- ip: source ip de l'event recupere
+- createdAt: date de création de l'event fournis ou recupere 
 	
 listen avec express
 app.get ('/api/events',
 app.get ('/api/events',
 app.post('/api/events/:num', )
-app.post('/api/events/:num', )
+app.get('/api/dashboard', )
 - GET /api/events (liste d'évenements)
 - POST /api/events (création d'un évenement)
 - GET /api/events/1 (récupération d'un évenement)
@@ -26,3 +41,11 @@ Pour le endpoint Dashboard, les événements sont représentés de la manière s
 	pas clair!! (interface graphique peut etre ??? possible bonus demande)
 
 modules : mongoose, express potentiellement cookie-session!
+*/
+const PORT = process.env.PORT || 3000
+	let server = app.listen(PORT)
+	console.log('App listens on port ' + PORT);
+	server.on('error', (err) =>
+	{
+		console.log (err)
+	})
